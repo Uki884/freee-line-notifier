e36f99b3b5f193bf26d9a6232ed1c0a6d0b3d9e7
Yuki Hayashi <mrchildreny@gmail.com>
2025-01-21 23:02:09 +0900

    Registrationコンポーネントにメッセージ送信機能を追加し、webhookの応答をボタンテンプレートに変更

--- apps/server/app/server.ts
+++ apps/server/app/server.ts
@@ -35,8 +35,22 @@ app.post("/webhook", async (c) => {
         if (message === "ログイン") {
           await client.replyMessage({
             replyToken: event.replyToken,
-            messages: [{ type: "text", text: CALLBACK_URL }],
-          });
+            messages: [{
+              type: 'template',
+              altText: 'Account Link',
+              template: {
+                type: 'buttons',
+                text: '連携開始を押してfreeeと連携してください',
+                actions: [
+                  {
+                    type: 'uri',
+                    label: '連携開始',
+                    uri: CALLBACK_URL
+                  },
+                ],
+              },
+            },
+          ]});
         }
       } catch (err: unknown) {
         if (err instanceof Error) {
